1ec072949f2b6661a3f8e46ef82c82ec6591ba5e
Abhishek210800 <abhishek.edu0721@gmail.com>
2025-01-19 20:01:34 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -5,6 +5,8 @@ import Nav from "./Nav";
 import Main from "./Main";
 import Footer from "./Footer";
 import About from "./Page/About";
+import Signup from "./Page/Signup";
+import Event from "./Page/Event";
 import Panalink from './Company/Panalink';
 
 
@@ -16,6 +18,8 @@ const Home = () => {
               <Route path="/" element={<Main />}></Route>
               <Route path="/Ebn" element={<Main />}></Route>
               <Route path="/About" element={<About />}></Route>
+              <Route path="/Event" element={<Event />}></Route>
+              <Route path="/Signup" element={<Signup/>}></Route>
               <Route path="/Panalink" element={<Panalink/>}></Route>
       </Routes>
       <Footer />
